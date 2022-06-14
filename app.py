d71d49781db6cae3199a4cc52507a9eaecb6c520
Natalia H <nholea@gmail.com>
2022-06-14 17:31:58 +0200

    Add see dog data feature

--- app.py
+++ app.py
@@ -11,16 +11,21 @@ def create_dog():
     dog = Dog(name, breed, birthdate, father_id, mother_id)
     dogs.append(dog)
 
-
 def show_all_dogs():
     return [dog.data_summary() for dog in dogs]
+
+def get_single_dog():
+    dog_id = int(input("Introduzca el identificador del perro que desea ver: "))
+    dog_found = [dog for dog in dogs if dog.id == dog_id]
+
+    return dog_found[0].serialize()
 
 
 def start():
     user_choice = input("""¿Qué desea hacer?:
     -Introduzca 1 para crear un nuevo registro de un perro
     -Introduzca 2 para mostrar listado perros registrados
-    -Introduzca 3 para  
+    -Introduzca 3 para ver datos de un perro
     -Introduzca 4 para ...
     -Pulsa ENTER sino quiere hacer nada 
     """)
@@ -33,10 +38,13 @@ def start():
         if user_choice == "2":
             print(show_all_dogs())
 
+        if user_choice == "3":
+            print(get_single_dog())
+
         user_choice = input("""¿Qué desea hacer?:
     -Introduzca 1 para crear un nuevo registro de un perro
     -Introduzca 2 para mostrar listado perros registrados
-    -Introduzca 3 para ...
+    -Introduzca 3 para ver datos de un perro
     -Introduzca 4 para ...
     -Pulsa ENTER sino quiere hacer nada 
     """)
