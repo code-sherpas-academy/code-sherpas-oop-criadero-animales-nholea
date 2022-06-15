543902da56fe6b7a07fdb37de0df43e58dfb851a
Natalia H <nholea@gmail.com>
2022-06-15 17:27:50 +0200

    Update dog data and create cat

--- Models.py
+++ Models.py
@@ -1,24 +1,50 @@
 import itertools
-from pickle import TRUE
 
-class Dog():
+
+class Animal():
     # Incremental ID
     newid = itertools.count()
 
-    def __init__(self, name, breed, birthdate, father = 0, mother = 0, tame= True):
+    def __init__(self, name, breed, birthdate):
         # Incremental ID
         self.id = next(self.newid)
         self. name = name
         self.breed = breed
         self. birthdate = birthdate
+
+
+class Dog(Animal):
+
+    def __init__(self, name, breed, birthdate, father = 0, mother = 0, tame = True):
+        super().__init__(name, breed, birthdate)
         self.father = father
         self.mother = mother
         self.tame = tame
-
+    
+    
     def serialize(self):
-        return {"id": self.id, "name": self.name, "breed": self.breed, "birthdate": self.birthdate, "father": self.father if self.father > 0 else "", "mother": self.mother if self.mother > 0 else "", "tame":self.tame}
+        return {"id": self.id, "name": self.name, "breed": self.breed, "birthdate": self.birthdate, "father": self.father if self.father > 0 else "", "mother": self.mother if self.mother > 0 else "", "tame": self.tame}
 
     def data_summary(self):
         return {"id": self.id, "name": self.name, "father": self.father if self.father > 0 else "", "mother": self.mother if self.mother > 0 else ""}
 
-dogs = []
+
+class Cat(Animal):
+
+    def __init__(self, name, breed, birthdate, father = 0, mother = 0, sociable = 1):
+        super().__init__(name, breed, birthdate)
+        self.father = father
+        self.mother = mother
+        self.sociable = sociable
+    
+    
+    def serialize(self):
+        return {"id": self.id, "name": self.name, "breed": self.breed, "birthdate": self.birthdate, "father": self.father if self.father > 0 else "", "mother": self.mother if self.mother > 0 else "", "sociable": self.sociable}
+
+    def data_summary(self):
+        return {"id": self.id, "name": self.name, "father": self.father if self.father > 0 else "", "mother": self.mother if self.mother > 0 else ""}
+
+
+
+
+animals = []
--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from pickle import FALSE
-from Models import Dog, dogs
-
+from Models import Dog, Cat,animals
 
 def create_dog():
     name = str(input("Introduzca nombre del perro: "))
@@ -13,27 +11,55 @@ def create_dog():
     tame = input(
         "Introduzca True si el perro está entrenado, False en caso contrario: ")
     dog = Dog(name, breed, birthdate, father_id, mother_id, tame)
-    dogs.append(dog)
+    animals.append(dog)
 
-
-def show_all_dogs():
-    return [dog.data_summary() for dog in dogs]
-
-
-def get_single_dog():
+def update_dog():
     dog_id = int(
-        input("Introduzca el identificador del perro que desea ver: "))
-    dog_found = [dog for dog in dogs if dog.id == dog_id]
+        input("Introduzca el identificador del perro que desea modificar: "))
+    dog_found = [dog for dog in animals if dog.id == dog_id]
+    dog_found[0].name = str(input("Introduzca nombre del perro: "))
+    dog_found[0].breed = str(input("Introduzca raza del perro: "))
+    dog_found[0].birthdate = str(input("Introduzca fecha de nacimiento: "))
+    dog_found[0].father = int(
+        input("Introduzca identificador del padre, si no lo conoce introduzca 0: "))
+    dog_found[0].mother = int(
+        input("Introduzca identificador de la madre,si no lo conoce introduzca 0: "))
+    dog_found[0].tame = input(
+        "Introduzca True si el perro está entrenado, False en caso contrario: ")
 
-    return dog_found[0].serialize()
+def create_cat():
+    name = str(input("Introduzca nombre del gato: "))
+    breed = str(input("Introduzca raza del gato: "))
+    birthdate = str(input("Introduzca fecha de nacimiento: "))
+    father_id = int(
+        input("Introduzca identificador del padre, si no lo conoce introduzca 0: "))
+    mother_id = int(
+        input("Introduzca identificador de la madre,si no lo conoce introduzca 0: "))
+    sociable = int(input(
+        "Introduzca un número del 1-5. 1 es «muy poco sociable» y 5 es «muy sociable»: "))
+    cat = Cat(name, breed, birthdate, father_id, mother_id, sociable)
+    animals.append(cat)
+
+
+def show_all_animals():
+    return [animal.data_summary() for animal in animals]
+
+
+def get_single_animal():
+    animal_id = int(
+        input("Introduzca el identificador del animal que desea ver: "))
+    animal_found = [animal for animal in animals if animal.id == animal_id]
+
+    return animal_found[0].serialize()
 
 
 def start():
     user_choice = input("""¿Qué desea hacer?:
     -Introduzca 1 para crear un nuevo registro de un perro
-    -Introduzca 2 para mostrar listado perros registrados
-    -Introduzca 3 para ver datos de un perro
-    -Introduzca 4 para ...
+    -Introduzca 2 para mostrar listado animales registrados
+    -Introduzca 3 para ver datos de un animal
+    -Introduzca 4 para modificar datos de un perro
+    Introduzca 5 para crear un nuevo registro de un gato
     -Pulsa ENTER sino quiere hacer nada 
     """)
 
@@ -43,16 +69,23 @@ def start():
             create_dog()
 
         if user_choice == "2":
-            print(show_all_dogs())
+            print(show_all_animals())
 
         if user_choice == "3":
-            print(get_single_dog())
+            print(get_single_animal())
+
+        if user_choice == "4":
+            update_dog()
+
+        if user_choice == "5":
+            create_cat()
 
         user_choice = input("""¿Qué desea hacer?:
     -Introduzca 1 para crear un nuevo registro de un perro
-    -Introduzca 2 para mostrar listado perros registrados
-    -Introduzca 3 para ver datos de un perro
-    -Introduzca 4 para ...
+    -Introduzca 2 para mostrar listado animales registrados
+    -Introduzca 3 para ver datos de un animal
+    -Introduzca 4 para para modificar datos de un perro
+    -Introduzca 5 para crear un nuevo registro de un gato
     -Pulsa ENTER sino quiere hacer nada 
     """)
 
